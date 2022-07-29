cae9ef6f103f69a0cc30966e58f82aec44fbdc02
maxwen <max.weninger@gmail.com>
2022-07-29 14:20:40 +0000

    kebab: Implement OnePlus Settings support
    Taken from https://github.com/AOSiP-Devices/device_oneplus_sm8150-common/commits/eleven/DeviceSettings.
    [cjh1249131356]:
    - Fully refractor, cleanup duplicated codes everywhere.
    - Write/Pick in more features.
    
    Includes:
    - Mute media volume on silent mode toggle
    - DC Dimming toggle + QS tile
    - HBM & Auto HBM + QS tile
    - Ambient display + QS tile (merged oplusdoze into OnePlusSettings)
    - FPS Info + QS tile
    - 5g modes switcher
    - High touch sample mode toggle (Force 240Hz) + QS tile
    - Tp Edge Limit toggle (Unlock edge touch) + QS tile
    - Vibrator strength preference
    - CustomSeekbarPreference & CustomSwitchPreference (Include haptic feedback)
    
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: LibXZR <xzr467706992@163.com>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ PRODUCT_PACKAGES += \
 
 # Alert slider
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key-calibrate
 
 # APEX
@@ -174,10 +173,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -356,6 +351,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     oplus-fwk
+
+# OnePlusSettings
+PRODUCT_PACKAGES += \
+    OnePlusSettings
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
