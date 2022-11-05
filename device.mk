06d2df0de2515ca26af8ed4389fcc1aa2bbd6ec7
johnmart19 <johnivan19999@gmail.com>
2022-11-05 03:19:43 +0200

    opkona: libstagefright: Build also softomx and foundation versions
    - Part of prebuilt blobs having them in deps:
    * libstagefright_softomx.so
    * libstagefright_foundation.so

--- device.mk
+++ device.mk
@@ -336,7 +336,9 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_foundation \
+    libstagefright_softomx
 
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
