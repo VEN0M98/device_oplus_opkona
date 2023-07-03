sodalime0509/device_oneplus_opkona

Decommonize and rebrand to opkona
- Merge the lemonades device tree[1] into common
- This includes proprietary files, overlays, props and touchscreen gesture config
- Rebrand it to opkona as we will be unifying all oneplus kona devices

[1] https://github.com/LineageOS/android_device_oneplus_lemonades

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/yaap_opkona.mk
+
+COMMON_LUNCH_CHOICES := \
+    yaap_opkona-user \
+    yaap_opkona-userdebug \
+    yaap_opkona-eng

VEN0M98/device_oplus_opkona

Signed-off-by: DawfukFR <DawfukFR@gmail.com>

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/arrow_opkona.mk
+
+COMMON_LUNCH_CHOICES := \
+    arrow_opkona-user \
+    arrow_opkona-userdebug \
+    arrow_opkona-eng