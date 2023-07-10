47a33d72f1bfd7826a4f7b4b5cbc9492387372db
VEN0M98 <bt98.bt7@gmail.com>
2023-07-10 09:10:17 +0200

    opkona:fixup for kInstallDeviceOpenError

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@
 #
 
 # A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
