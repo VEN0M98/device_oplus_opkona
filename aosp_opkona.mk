b2cc1de5b775d016831ca16e8f7d975ea2ce48e1
VEN0M98 <bt98.bt7@gmail.com>
2023-07-11 20:26:37 +0200

    blki-ify

--- aosp_opkona.mk
+++ aosp_opkona.mk
@@ -11,17 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from opkona device
 $(call inherit-product, device/oneplus/opkona/device.mk)
 
-# Inherit some common arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_opkona
+PRODUCT_NAME := aosp_opkona
 PRODUCT_DEVICE := opkona
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := kona
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-# ArrowOS stuff
-ARROW_GAPPS := true
+# aospOS stuff
+TARGET_GAPPS_ARCH := arm64
+FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_HAS_UDFPS := true
+BLKI_BUILD_TYPE := OFFICIAL
+
+# Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := DawfukFR
